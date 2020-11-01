8b0760bae97079081f3face99993de5a587cda03
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-10-31 17:51:38 -0700

    updated comment

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ mongoose
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const port = process.env.PORT || 5000;
-//localhost
+//localhost:5000//
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
